fb129ef075804a9e2986f35243e3fa457b931ccb
mohammed2950 <52863840+mohammed2950@users.noreply.github.com>
2020-05-31 22:30:08 -0400

    files added to the project

---
+++ app/src/main/java/com/arifali/e_commerce_app/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.arifali.e_commerce_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.paperdb.Paper;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button LogoutBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        LogoutBtn = (Button) findViewById(R.id.btn_logout);
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Paper.book().destroy();
+
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/arifali/e_commerce_app/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.arifali.e_commerce_app.Prevalent;
+
+import com.arifali.e_commerce_app.Model.Users;
+
+public class Prevalent {
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
